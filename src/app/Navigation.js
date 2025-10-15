8636dad4b9f4478d2ec822caf256fa7a9fc4c8aa
김정민 <qhddkvoalffl@naver.com>
2025-10-15 18:17:03 +0900

    li에 쓸데없이 들어간 속성 제거

--- src/app/Navigation.js
+++ src/app/Navigation.js
@@ -7,15 +7,15 @@ export default function Navigation() {
         <h1 className="text-2xl font-bold text-blue-400 glitch-text ">이그나이트</h1>
         <nav>
           <ul className="flex space-x-4 md:space-x-3 text-gray-300 text-sm md:text-base">
-            <li className='block'><Link href="./" 
+            <li><Link href="./" 
             className="inline-block p-2 rounded-md hover:bg-gray-700/70 hover:text-blue-300 hover:-translate-y-1 transition-transform duration-300">홈</Link></li>
-            <li className='block'><Link href="about" 
+            <li><Link href="about" 
             className="inline-block p-2 rounded-md hover:bg-gray-700/70 hover:text-blue-300 hover:-translate-y-1 transition-transform duration-300">소개</Link></li>
-            <li className='block'><Link href="products" 
+            <li><Link href="products" 
             className="inline-block p-2 rounded-md hover:bg-gray-700/70 hover:text-blue-300 hover:-translate-y-1 transition-transform duration-300">제품</Link></li>
-            <li className='block'><Link href="community" 
+            <li><Link href="community" 
             className="p-2 inline-block rounded-md hover:bg-gray-700/70 hover:text-blue-300 hover:-translate-y-1 transition-transform duration-300">커뮤니티</Link></li>
-            <li className='block'><Link href="contact" 
+            <li><Link href="contact" 
             className="p-2 inline-block rounded-md hover:bg-gray-700/70 hover:text-blue-300 hover:-translate-y-1 transition-transform duration-300">고객센터</Link></li>
         </ul>
         </nav>
