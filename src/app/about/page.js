09168152a3b88d9db19068631d3bdd7c274a953e
김정민 <qhddkvoalffl@naver.com>
2025-11-03 19:25:39 +0900

    강사님 헬프로 소개탭 레이아웃 수정 완료

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -15,9 +15,9 @@ export async function generateMetadata() {
 export default function TeamPage() {
 
   return (
-    <div className="AboutBody flex flex-row justify-center bg-gray-50" >
+    <div className="AboutBody flex flex-row justify-center bg-gray-50 h-screen overflow-hidden" >
       <SidebarOfMemberNav />
-      <div className="AboutContentBox flex flex-col max-w-14/15 mx-auto py-10 md:14 px-4 sm:px-6 lg:px-8">
+      <div className="AboutContentBox flex flex-col max-w-14/15 mx-auto py-10 md:14 px-4 sm:px-6 lg:px-8 felx-1 overflow-y-auto">
 
         
         {/* 📌 페이지 제목 */}
